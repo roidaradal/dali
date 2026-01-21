8826a4b63130509db3bd1ec96e1267e1710813fb
roidaradal <johnroy.daradal@goodkredit.com>
2026-01-21 14:59:11 +0800

    Use io.GetCommandOptions

--- main.go
+++ main.go
@@ -3,15 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"strings"
 
 	"github.com/roidaradal/dali/internal/dali"
-	"github.com/roidaradal/fn/dict"
+	"github.com/roidaradal/fn/io"
 )
 
 func main() {
-	command, options := getCommandArgs()
+	command, options := io.GetCommandOptions(dali.HelpCmd)
 
 	node, err := dali.LoadNode(command)
 	if err != nil {
@@ -27,24 +25,4 @@ func main() {
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
-}
-
-// Get command and options
-func getCommandArgs() (string, dict.StringMap) {
-	args := os.Args[1:]
-	if len(args) < 1 {
-		args = []string{dali.HelpCmd}
-	}
-	command := strings.ToLower(args[0])
-	options := make(dict.StringMap)
-	for _, pair := range args[1:] {
-		parts := strings.Split(pair, "=")
-		key := strings.ToLower(parts[0])
-		value := ""
-		if len(parts) == 2 {
-			value = parts[1]
-		}
-		options[key] = value
-	}
-	return command, options
 }
